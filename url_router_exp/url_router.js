3993d712022daa4f4a9753b2f181275455b836c4
fdsilva <neitonfsilva@gmail.com>
2016-05-13 00:40:11 -0700

    Solving bugs on url_routers.js

--- url_router_exp/url_router.js
+++ url_router_exp/url_router.js
@@ -3,29 +3,41 @@ var fs = require('fs');
 var url = require('url');
 
 var callbackServer = function(request, response){
-	
-	response.writeHeader(200, {'Content-Type': 'text/html'});
+		
 	var result = url.parse(request.url).pathname;
+	response.writeHeader(200, {'Content-Type': 'text/html'});
 
-	if(result == "/" /*&& result == "/main"*/){
-		//response.write("<h1>Teste</h1>");
-		fs.readFile(__dirname + '/main.html', function(error, html){
+	if(result == "/" || result == "/main"){
+		fs.readFile(__dirname + '/main.html', function(err, html){
+			
+			if(err){
+				throw err;
+			}
+
 			response.write(html);
+			response.end();
 		});
 	
 	}else if(result == "/contact"){
-		fs.readFile(__dirname+'/contact.html', function(error, html){
+		fs.readFile('./contact.html', function(err, html){
+			if(err){
+				throw err;
+			}
+
 			response.write(html);
+			response.end();
 		});
 	
 	}else{
-		fs.readFile(__dirname+'/error.html', function(error, html){
+		fs.readFile(__dirname+'/error.html', function(err, html){
+			if(err){
+				throw err;
+			}
+			
 			response.write(html);
+			response.end();
 		});
-
 	}
-
-	response.end();
 }
 
 var callbackLog = function(){
@@ -34,4 +46,3 @@ var callbackLog = function(){
 
 var service = http.createServer(callbackServer);
 service.listen(3000, callbackLog);
-
